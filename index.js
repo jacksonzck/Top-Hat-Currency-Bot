43188bf1d124f16c9ba4d65a5d3c5e54262bf864
detterbeckj <detterbeckjackson@gmail.com>
2021-08-04 20:25:11 -0500

    Forgot to commit this :/

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
 const https = require('https');
+const fs = require('fs');
 dotenv.config();
 const client = new Discord.Client();
+
+client.once('ready', () => {
+    fs.open('transactions.txt', 'w+', function () {});
+    fs.open('accountBalances.txt', 'w+', function () {});
+})
+
 client.login(process.env.DISCORD_TOKEN);
+
+
+
+client.on('message', message => {
+    if(message.content == "!ping") {
+        message.reply("pong");
+    }
+    if(message.content == "!test") {
+        Account.findNumber("Wow");
+    }
+})
+
+const Account = {
+    findNumber(name) {
+        fs.readFileSync("accountBalances.txt")
+    }
+}
